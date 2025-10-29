9d7c0ae6c9ab129598d44e01b0bd484571adae17
avery-byo <allicia.sagi@gmail.com>
2025-10-29 13:28:14 +0700

    Initial Commit

---
+++ Mark2.0/Mark2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Mark2_0App.swift
+//  Mark2.0
+//
+//  Created by Allicia Viona Sagi on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Mark2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
